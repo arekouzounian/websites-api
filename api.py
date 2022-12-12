ecdb7a580dead5ee0efac61e6742beccdc455faa
Arek Ouzounian <agouzo777@gmail.com>
2022-12-12 02:54:48 -0800

    initial commit

---
+++ api.py
@@ -0,0 +1,33 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/api/v1/test')
+def test():
+    test = {"key": "value", "test": 1}
+    return jsonify(test)
+
+@app.route('/api/v1/interests', methods=['GET'])
+def interests():
+    interests = []
+    with open('./data/interests.txt') as f:
+        for line in f.readlines():
+            interests.append(line)
+    return jsonify(interests), 200
+
+@app.route('/api/v1/about', methods=['GET'])
+def about(): 
+    about = []
+    with open('./data/about.txt') as f: 
+        curr = ''
+        for line in f.readlines():
+            if line == '\n':
+                if len(curr) > 1: 
+                    about.append(curr)
+                    curr = ''
+            else: 
+                curr += line
+    return jsonify(about) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
